75940e9f13b83aa9bb22e30d882bab29f578bff8
Yuanjun Ren <renyj@buaa.edu.cn>
2022-08-03 00:02:28 +0800

    Add EOF parser for space parsing

--- main.cc
+++ main.cc
@@ -1,139 +1,121 @@
 #include <iostream>
 #include <memory>
 #include <stack>
+#include <stdarg.h>
 #include <string>
 #include <vector>
 
-class Operator {
-public:
-  Operator();
-  enum class Types {
-    PLUS,
-    MINUS,
-    MUL,
-    DIV,
-    NONE,
-  };
-
-  void dump() {
-  }
-
-private:
+enum class TokenKind {
+  TK_PUNCT, // operator. + -
+  TK_NUM,   // number
+  TK_EOF,   // end of file
 };
 
-template<typename T>
-class Value {
-  T val;
-
-public:
-  void dump() {
-#ifndef NDEBUG
-    print(std::cout);
-#endif
-  }
-
-private:
-  void print(std::ostream &os) {
-    os << "VAL: " << val;
-  }
+struct Token {
+  TokenKind Kind;             // kind
+  Token *Next;                // next token
+  int Val;                    // value
+  std::string::iterator Loc;  // location
+  size_t Len;                 // length
 };
 
-template <typename T>
-class Instruction {
-  std::unique_ptr<Operator> op;
-  std::unique_ptr<Value<T>> val1;
-  std::unique_ptr<Value<T>> val2;
-
-  void dump() {
-#ifndef NDEBUG
-    this->print(std::cout);
-#endif
-  }
-
-private:
-  void print(std::ostream &os) {
-    val1->dump();
-    op->dump();
-    val2->dump();
-  }
-};
-
-class Parser {
-  std::vector<std::string> commands;
-  std::string raw_input;
-
-public:
-  Parser() = default;
-
-  Parser &operator=(const Parser &) = delete;
-
-  static Parser &instance();
-
-  bool parse(std::string &inputs);
-};
-
-static Parser *parser = nullptr;
-
-Parser &Parser::instance() {
-  if (parser == nullptr) {
-    parser = new Parser();
-  }
-
-  return *parser;
+static void error(char *Fmt, ...) {
+  // define a va_list variable.
+  va_list VA;
+  // get all arguments after Fmt.
+  va_start(VA, Fmt);
+  // output va_list variable using vfprintf.
+  vfprintf(stderr, Fmt, VA);
+  // add line separator.
+  fprintf(stderr, "\n");
+  // clear VA
+  va_end(VA);
+  // terminate program.
+  std::exit(1);
 }
 
-bool Parser::parse(std::string &inputs) {
-  if (inputs.size() == 0)
-    return false;
+static int getNumber(Token &Tok) {
+  if (Tok.Kind != TokenKind::TK_NUM)
+    error("expect a number.");
+  return Tok.Val;
+}
 
-#ifndef NDEBUG
-  std::cout << inputs << std::endl;
-#endif
+static Token *newToken(TokenKind Kind,
+                       std::string::iterator Start,
+                       std::string::iterator End) {
+  auto *Tok = new Token;
+  Tok->Kind = Kind;
+  Tok->Loc = Start;
+  Tok->Len = std::distance(Start, End);
+  return Tok;
+}
 
-  std::stack<Operator::Types> opPool;
-  std::stack<int> valPool;
-  int val = 0;
-  Operator::Types op = Operator::Types::NONE;
-
-  auto recordVal = [&](Operator::Types type) {
-    opPool.push(type);
-    valPool.push(val);
-    val = 0;
-  };
-
-  for (auto achar : inputs) {
-    if (std::isspace(achar))
-      continue;
-
-    switch (achar) {
-    case '+': {
-      recordVal(Operator::Types::PLUS);
-      break;
-    }
-    case '-': {
-      recordVal(Operator::Types::MINUS);
-      break;
-    }
-    case '*': {
-      recordVal(Operator::Types::MUL);
-      break;
-    }
-    case '/': {
-      recordVal(Operator::Types::DIV);
-      break;
-    }
-    default: {
-      if (std::isdigit(achar))
-        val = val * 10 + achar - '0';
-      else if (std::isalpha(achar)) {
-        std::cout << "Unsupported yet." << std::endl;
-        ::abort();
-      }
-      break;
-    }
-    }
+std::string::iterator getNumberPos(std::string &input, std::string::iterator Start) {
+  auto End = input.end();
+  for (; Start != End; ++Start) {
+    char achar = *Start;
+    if (!std::isdigit(achar))
+      return Start;
   }
-  return true;
+  return Start;
+}
+
+// EOF parser.
+static Token *tokenize(std::string &input) {
+  Token Head{};
+  Token *Cur = &Head;
+  auto It = input.begin();
+
+  for (auto End = input.end(); It != End;) {
+    char achar = *It;
+
+    // skip spaces
+    if (std::isspace(achar)) {
+      ++It;
+      continue;
+    }
+
+    // parse number.
+    if (std::isdigit(achar)) {
+      Cur->Next = newToken(TokenKind::TK_NUM, It, It);
+      Cur = Cur->Next;
+
+      auto NumEndPos = getNumberPos(input, It);
+      Cur->Val = std::stoi(input.substr(std::distance(input.begin(), It),
+                                        std::distance(It, NumEndPos)));
+      Cur->Len = std::distance(It, NumEndPos);
+      It = NumEndPos;
+      continue;
+    }
+
+    // parse operators.
+    if (achar == '+' || achar == '-') {
+      Cur->Next = newToken(TokenKind::TK_PUNCT, It, It + 1);
+      Cur = Cur->Next;
+      ++It;
+      continue;
+    }
+
+    // unknown character.
+    error("invalid token: %c", achar);
+  }
+
+  // Add EOF operator.
+  Cur->Next = newToken(TokenKind::TK_EOF, It, It);
+
+  return Head.Next;
+}
+
+static bool equal(Token *Tok, std::string Str) {
+  // compare LHS and RHS, if S2
+  return std::equal(Tok->Loc, Tok->Loc + Tok->Len, Str.begin());
+}
+
+// skip specified string
+static Token *skip(Token *Tok, std::string Str) {
+  if (!equal(Tok, Str))
+    error("expect %s", Str.data());
+  return Tok->Next;
 }
 
 int main(int argc, char **argv) {
@@ -141,70 +123,31 @@ int main(int argc, char **argv) {
     std::cerr << "Invalid arguments.\n";
     std::cerr << "Usage:\n";
     std::cerr << "\t ./exec val\n";
-    return 1;
+    error("%s: Invalid number of arguments", argv[0]);
   }
+
+  std::string input(argv[1]);
+  auto *Tok = tokenize(input);
 
   // header
   std::cout << ".globl main\n";
   std::cout << "main:\n";
+  std::cout << "  li a0, " << getNumber(*Tok) << std::endl;
+  Tok = Tok->Next;
 
-
-  std::string delims{" +-"};
-  std::string input_val = std::string{argv[1]};
-  std::string::size_type begIdx, endIdx;
-  std::string::size_type size = input_val.size();
-
-  auto isNumber = [=](std::string const &input,
-          const std::string::size_type start,
-          const std::string::size_type end)-> bool {
-    auto it = std::find_if(input.begin() + start, input.begin() + end,
-                           [=](char const &c) -> bool {
-      return !std::isdigit(c);
-    });
-    return !input.empty() && it == (input.begin() + end) && std::isdigit(input[end - 1]);
-  };
-
-  // 这里我们将算式分解为 num(op num) (op num) ...  的形式
-  endIdx = input_val.find_first_of(delims);
-  if (!isNumber(input_val, 0, endIdx == std::string::npos ? size : endIdx)) {
-    std::cerr << "This is not a number: " << input_val.substr(0, endIdx) << std::endl;
-    std::exit(1);
-  }
-
-    std::cout << "  li a0, "
-              << input_val.substr(0, endIdx == std::string::npos ? size : endIdx)
-              << std::endl;
-
-  begIdx = endIdx;
-  while (begIdx != std::string::npos) {
-    if (input_val[begIdx] == '+' || input_val[begIdx] == '-') {
-      bool negative_flag = input_val[begIdx] == '-';
-      ++begIdx;
-
-      bool endflag = false;
-      endIdx = input_val.find_first_of(delims, begIdx);
-      if (endIdx == std::string::npos) {
-        endflag = true;
-        endIdx = size;
-      }
-
-      if (!isNumber(input_val, begIdx, endIdx)) {
-        std::cerr << "This is not a number: " << input_val.substr(begIdx, endIdx) << std::endl;
-        std::exit(1);
-      }
-
-      std::cout << "  addi a0, a0, "
-                << (negative_flag ? "-" : "")
-                << input_val.substr(begIdx, endIdx - begIdx) << std::endl;
-
-      begIdx =  endflag ? std::string::npos : endIdx;
+  while (Tok->Kind != TokenKind::TK_EOF) {
+    if (equal(Tok, "+")) {
+      Tok = Tok->Next;
+      std::cout << "  addi a0, a0, " << getNumber(*Tok) << std::endl;
+      Tok = Tok->Next;
       continue;
     }
 
-    std::cerr << "  Unsupported character: " << input_val[begIdx] << std::endl;
-    std::exit(1);
+    // if no subi instr,
+    Tok = skip(Tok, "-");
+    std::cout << "  addi a0, a0, -" << getNumber(*Tok) << std::endl;
+    Tok = Tok->Next;
   }
-
   // ret为jalr x0, x1, 0 别名指令，用于返回子程序
   std::cout << "  ret\n";
 
